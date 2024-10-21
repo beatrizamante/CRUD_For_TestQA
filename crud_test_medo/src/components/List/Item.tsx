443bca621dfc5a30dadd1b315e38456fff217c33
Beatriz <beatrizamante@hotmail.com>
2024-10-21 19:54:51 -0300

    Duration

--- crud_test_medo/src/components/List/Item.tsx
+++ crud_test_medo/src/components/List/Item.tsx
@@ -4,7 +4,7 @@ export default function Item({ band, album }: {band: string, album: string}) {
     
   return (
     <li
-    className="relative mb-4 pb-2 text-lighter text-xl after:content-[''] after:block after:w-full after:h-[1px] after:bg-lighter after:absolute after:left-0 after:bottom-0 flex flex-row justify-center hover:text-slate-400"
+    className="relative mb-4 pb-2 text-lighter text-xl after:content-[''] after:block after:w-full after:h-[1px] after:bg-lighter after:absolute after:left-0 after:bottom-0 flex flex-row justify-center hover:text-slate-400 duration-150"
     >
       <h6 className="mx-4 mb-2 text-ellipsis">{band} - {album}</h6>
     </li>
