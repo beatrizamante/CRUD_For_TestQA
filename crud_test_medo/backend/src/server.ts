54ab68e498eedc84b359208f027b4880a41a6f48
Beatriz <beatrizamante@hotmail.com>
2024-10-23 08:09:00 -0300

    Ajustes

--- crud_test_medo/backend/src/server.ts
+++ crud_test_medo/backend/src/server.ts
@@ -1,15 +1,19 @@
-  import { initializeDataSource } from './connection';
-  import express from 'express';
-  import router from './routes';
-  
-  const app = express();
-  const PORT = 3001;
-  app.use(express.json());
-  app.use('/api', router);
-  initializeDataSource().then(() => {
-      app.listen(PORT, () => {
-          console.log(`Server running on port ${PORT}`);
-      });
-  }).catch((error) => {
-      console.error("Failed to initialize data source:", error);
+import { initializeDataSource } from "./connection";
+import express from "express";
+import router from "./routes";
+
+const app = express();
+const PORT = 3001;
+app.use(express.json());
+app.use("/api", router);
+initializeDataSource()
+  .then(() => {
+    console.log(`Initializing.........`);
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to initialize data source:", error);
   });
